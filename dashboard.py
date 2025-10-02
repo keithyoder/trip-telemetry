5cccd2e9d00975cbefb764432c20c6f203368508
keithyoder <yoder@tessi.com.br>
2025-10-02 14:09:20 -0300

    add async thread

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,8 @@
 from dash import Dash, html, dcc, Input, Output, callback
 import dash_daq as daq
 import time
+import asyncio
+import threading
 from sensors.bmp581 import BMP581
 bmp581 = BMP581(1019)
 app = Dash()
@@ -41,9 +43,21 @@ def update_output(n):
         pressure = f"{bmp581.values['bmp581_pressure_hPa']:.1f}"
     return [temperature, pressure]
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+async def read_sensors_async():
     while True:
         bmp581.read()
-        time.sleep(1)
+        await asyncio.sleep(1)  # Read every 1 second
 
+def run_async_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever(read_sensors_async())
+
+if __name__ == '__main__':
+    new_loop = asyncio.new_event_loop()
+    
+    # Start the async sensor reading in a separate thread
+    async_thread = threading.Thread(target=run_async_loop, args=(new_loop,))
+    async_thread.daemon = True # Allow the main program to exit even if this thread is running
+    async_thread.start()
+
+    app.run(host='0.0.0.0', debug=True)
