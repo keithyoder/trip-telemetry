039394f8815ed96703e395ed6199502e2a049df6
keithyoder <yoder@tessi.com.br>
2025-10-05 14:56:06 -0300

    add mongodb logger

--- loggers/mongodb.py
+++ loggers/mongodb.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 class MongoDBLogger:
     def __init__(self):
         self.client = MongoClient('localhost', 27017)
-        self.db = self.client.pi_i2c_logger
-        self.collection = self.db.logs
+        self.db = self.client['pi_i2c_logger']
+        self.collection = self.db['logs']
 
     def close(self):
         self.client.close()
